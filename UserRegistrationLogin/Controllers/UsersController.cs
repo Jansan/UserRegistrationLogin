1ca73c222f5732a00a92478615bc87955a0f6c8d
Jansan <jansan-26@hotmail.com>
2019-05-12 14:41:35 +0200

    registration view create

---
+++ UserRegistrationLogin/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserRegistrationLogin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // Registration Action
+        [HttpPost]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        //Registration POST action
+
+        //Verify Email
+
+        //Veryfy Email LINK
+
+        //Login
+
+        //Login POST
+    }
+}
